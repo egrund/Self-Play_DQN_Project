50842e43ff9da1b9ee092101a5eda5d81a594c77
egrund <82370915+egrund@users.noreply.github.com>
2023-03-03 16:19:13 +0100

    update 2

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from keras_gym_env import ConnectFourEnv
 
 from agent import DQNAgent
 from buffer import Buffer
+from sampler import Sampler
 
 # Hyperparameter
-
+BATCH_SIZE = 16
 
 # create environment
 env = ConnectFourEnv()
@@ -12,7 +13,10 @@ actions = env.available_actions
 
 # create buffer
 buffer = Buffer()
-buffer.fill()
 
 # create agent
 agent = DQNAgent(env,buffer)
+
+# create Sampler
+sampler = Sampler(BATCH_SIZE,agent)
+buffer.fill()
--- sampler.py
+++ sampler.py
@@ -1,22 +1,22 @@
-
+from keras_gym_env import ConnectFourEnv
 
 class Sampler:
     """ 
     
     """
 
-    def __init__(self,env,model):
+    def __init__(self,batch,agent):
 
-        self.env = env
-        self.model = model
+        self.envs = [ConnectFourEnv() for _ in range(batch)]
+        self.agent = agent
 
     def sample_from_game(self):
         """ """
         sarsa = list()
 
-        action_space_size = self.env.action_space.n
-        observations = [self.env.reset()]
+        action_space_size = self.envs[0].action_space.n
+        observations = [env.reset() for env in self.envs]
 
         for _ in range(10000):
 
-            
+            actions = self.agent

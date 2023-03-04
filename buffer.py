ef05bb3ba1912c777f185bfb286f80e0a2ed705a
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-03-04 16:21:10 +0100

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -7,16 +7,11 @@ class Buffer:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.s = None
-        self.a = None
-        self.r = None
-        self.s_n = None
-        self.done = None
-        self.sarsd_list = [[self.s, self.a, self.r, self.s_n, self.done] for i in range(capacity)]
+        self.sarsd_list = []
         self.idx = 0
 
-    def extend(self, state, action, reward, new_state, done):
-        """ adds new data to the circular memory buffer """
+    def extend(self, sarsd):
+        """ adds new data to memory buffer """
         """
         Prameters:
         ______________
@@ -33,15 +28,19 @@ class Buffer:
         none
         ______________
         """
-        #sarsd_list[idx][state, action, reward, next_state, done]]
-        self.sarsd_list[self.idx][0] = state
-        self.sarsd_list[self.idx][1] = action
-        self.sarsd_list[self.idx][2] = reward
-        self.sarsd_list[self.idx][3] = next_state     
-        self.sarsd_list[self.idx][4] = done
+        #extend till the capacity is reached
+        if(len(sarsd_list) < capacity):
+            self.sarsd_list.extend(sarsd)
+            
+        else:
+            self.idx = rnd.randint(0, capacity - 1) #random overwrite
+            self.sarsd_list[self.idx][0] = sarsd[0] #state
+            self.sarsd_list[self.idx][1] = sarsd[1] #action
+            self.sarsd_list[self.idx][2] = sarsd[2] #reward
+            self.sarsd_list[self.idx][3] = sarsd[3] #next_state
+            self.sarsd_list[self.idx][4] = sarsd[4] #done
         
-        #for circular memory
-        self.idx = (self.idx+1)%self.capacity
+        
 
     def sample_minibatch(self, batch_size):
         """ samples a random minibatch from the buffer """
@@ -57,6 +56,6 @@ class Buffer:
         sample(list) = A sample pulled from sarsd_list containing [state, action, reward, next_state, done]
         ______________
         """
-        sample = rnd.sample(sarsd_list, batch_size)
+        sample = rnd.sample(self.sarsd_list, batch_size)
         
         return sample

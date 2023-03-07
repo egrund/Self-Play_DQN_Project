0b3885fb31ddac783dea50cff5780d8e6b1a4476
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-03-07 12:50:33 +0100

    Update and adding sampler

--- training.py
+++ training.py
@@ -1,17 +1,38 @@
-def train(self,iterations : int, path_save_weights : str, path_save_logs : str):
+def train(self, agents, iterations : int, summary_writer, EPSILON = 1, EPSILON_DECAY = 0.9):
         """ """
 
-        for i in range(iterations):
+    
+    
+    # create Sampler
+    sampler = Sampler(BATCH_SIZE, agents)
+    sampler.fill_buffers(EPSILON)
+        
+    for i in range(iterations):
+        
+        # epsilon decay
+        EPSILON = EPSILON * EPSILON_DECAY
 
-            # epsilon decay
+        # train the dqn + new samples
+        if len(agents)==len(train_summary_writer):
+            [agent.train_inner_iteration(self, train_summary_writer[j], i) for j, agent in enumerate(agents)]
+        else:
+            raise IndexError("You need the same amount of summary writers and agents.")
 
-            # train the dqn + new sampels
-            self.train_episode(i)
+        # new sampling + add to buffer
+        sampler.sample_from_game(epsilon)
 
-            # new sampling + add to buffer
+        # write summary
+        # create directory for logs
+            
+        # logging the validation metrics to the log file which is used by tensorboard
+        #with train_summary_writer.as_default():
+        #    for metric in model.metrics:
+        #        tf.summary.scalar(f"{metric.name}", metric.result(), step=epoch)
 
-            #buffer.extend()
+        # reset all metrics
+        model.reset_metrics()
+        print("\n")
 
-            # write summary
-
-            print("Iteration: ", i)
+        # save model
+        if iterations%10 == 0:
+            model.save_weights(f"model/{config_name}/{epoch}")

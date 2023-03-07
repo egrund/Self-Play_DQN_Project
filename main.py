3f4d6195c303b0fff1f9f88f01f57edae034213c
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-03-07 14:27:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from keras_gym_env import ConnectFourEnv
 import numpy as np
+import datetime
+import tensorflow as tf
 
 from agent import DQNAgent
 from buffer import Buffer
-from sampler import Sampler
 from training import train
 
 #Subfolder for Logs
 config_name = "default"
-
 #createsummary writer for vusalization in tensorboard    
 time_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 best_train_path = f"logs/{config_name}/{time_string}/best_train"
-adapting_train_patch = = f"logs/{config_name}/{time_string}/adapting_train"
+adapting_train_path = f"logs/{config_name}/{time_string}/adapting_train"
 best_train_writer = tf.summary.create_file_writer(best_train_path)
 adapting_train_writer = tf.summary.create_file_writer(adapting_train_path)
 
-best_test_path = f"logs/{config_name}/{time_string}/best_train"
-adapting_test_patch = = f"logs/{config_name}/{time_string}/adapting_train"
+best_test_path = f"logs/{config_name}/{time_string}/best_test"
+adapting_test_path = f"logs/{config_name}/{time_string}/adapting_test"
 best_test_writer = tf.summary.create_file_writer(best_test_path)
 adapting_test_writer = tf.summary.create_file_writer(adapting_test_path)
 
@@ -26,6 +26,7 @@ train_writer = [best_train_writer, adapting_train_writer]
 test_writer = [best_test_writer, adapting_test_writer]
 
 # Hyperparameter
+iterations = 12
 BATCH_SIZE = 4
 reward_function_adapting_agent = lambda d,r: np.where(r==0,[1,0]) if d else r
 EPSILON = 0.01 #TODO
@@ -36,11 +37,11 @@ adapting_buffer = Buffer(100000,1000)
 
 # create agent
 env = ConnectFourEnv()
-best_agent = DQNAgent(env,best_buffer)
-adapting_agent = DQNAgent(env, adapting_buffer, reward_function = reward_function_adapting_agent)
+best_agent = DQNAgent(env,best_buffer, batch = 4)
+adapting_agent = DQNAgent(env, adapting_buffer, batch = 4, reward_function = reward_function_adapting_agent)
 agents = [best_agent, adapting_agent]
 
-training(agents, iterations, train_writer, test_writer)
+train(agents, BATCH_SIZE, iterations, train_writer, test_writer, EPSILON = 1, EPSILON_DECAY = 0.9)
 
 
 #states = sampler.sample_from_game(EPSILON)

1277915108499c5fcbc974ab0fff7d91e0ca77d0
egrund <82370915+egrund@users.noreply.github.com>
2023-03-07 14:09:40 +0100

    dqn train step

--- model.py
+++ model.py
@@ -20,8 +20,9 @@ class MyMLP(tf.keras.Model):
         self.optimizer = optimizer
         self.loss = loss
         self.output_units = output_units
+        self.model_target = None
 
-        self.metric = [tf.keras.metrics.Mean(name="loss"), tf.keras.metrics.CategoricalAccuracy(name="accuracy")]
+        self.metric = [tf.keras.metrics.Mean(name="loss")]
 
     def reset_metrics(self):
         """ resets all the metrices that are observed during training and testing """
@@ -29,9 +30,9 @@ class MyMLP(tf.keras.Model):
             m.reset_states()
 
     @tf.function
-    def call(self, inputs):
+    def call(self, states):
         """ forward propagation of the ANN """
-        x = tf.keras.layers.Flatten()(inputs)
+        x = tf.keras.layers.Flatten()(states)
         for layer in self.dense_list:
             x = layer(x)
         x = self.out(x)
@@ -39,42 +40,33 @@ class MyMLP(tf.keras.Model):
 
     @tf.function
     def train_step(self, inputs):
+        """ 
+        one train step of the model
 
-        images, targets = inputs
+        inputs (list): state, action, reward, new_state, done (axis 0 is the batch dim)
+        """
+
+        if not self.model_target:
+            raise Exception("This model does not have a target model.")
+
+        s,a,r,s_new, done = inputs
 
         with tf.GradientTape() as tape: 
+            
+            # calculate the target Q value
+            Qmax = tf.math.reduce_max(self.model_target(s_new),axis=1)
+            # calculate q value of this state action pair
+            Qsa = tf.gather(self(s),indices = tf.cast(a,tf.int32),axis=1,batch_dims=1)
 
-            predictions = self(images)
-            losses = self.loss(targets, predictions)
+            losses = self.loss(Qsa, r + (tf.constant(0.99)*Qmax)*(1-done))
 
             self.metric[0].update_state(value=losses)
-            self.metric[1].update_state(predictions, targets)
 
         # get the gradients
         gradients = tape.gradient(losses,self.trainable_variables)
 
         # apply the gradient
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-
-        return{m.name : m.result() for m in self.metric}
-
-    @tf.function
-    def test_step(self,data):
-        """ does one test step in one episode given a batch of data 
-        
-        Parameters: 
-            data (tuple) = shape (img, target)
-        
-        returns a dictionary of the metrics
-        """
-
-        img, targets = data
-
-        predictions = self(img,training=False)
-        loss = self.loss_function(targets, tf.squeeze(predictions))
-
-        self.metric[0].update_state(values = loss) # loss
-        self.metric[1].update_state(predictions, targets) # accuracy
 
         return{m.name : m.result() for m in self.metric}
 

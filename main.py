cb1fe66414fff885df21642679e5d06f6ed34542
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-03-07 13:02:13 +0100

    Adding writers

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ config_name = "default"
 
 #createsummary writer for vusalization in tensorboard    
 time_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-train_file_path = f"logs/{config_name}/{time_string}/train"
-test_file_path = f"logs/{config_name}/{time_string}/test"
-train_summary_writer = tf.summary.create_file_writer(train_file_path)
-test_summary_writer = tf.summary.create_file_writer(test_file_path)
+
+best_train_path = f"logs/{config_name}/{time_string}/best_train"
+adapting_train_patch = = f"logs/{config_name}/{time_string}/adapting_train"
+best_train_writer = tf.summary.create_file_writer(best_train_path)
+adapting_train_writer = tf.summary.create_file_writer(adapting_train_path)
+
+best_test_path = f"logs/{config_name}/{time_string}/best_train"
+adapting_test_patch = = f"logs/{config_name}/{time_string}/adapting_train"
+best_test_writer = tf.summary.create_file_writer(best_test_path)
+adapting_test_writer = tf.summary.create_file_writer(adapting_test_path)
+
+train_writer = [best_train_writer, adapting_train_writer]
+test_writer = [best_test_writer, adapting_test_writer]
 
 # Hyperparameter
 BATCH_SIZE = 4
@@ -31,6 +40,7 @@ best_agent = DQNAgent(env,best_buffer)
 adapting_agent = DQNAgent(env, adapting_buffer, reward_function = reward_function_adapting_agent)
 agents = [best_agent, adapting_agent]
 
+training(agents, iterations, train_writer, test_writer)
 
 
 #states = sampler.sample_from_game(EPSILON)

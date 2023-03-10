85958abd850b69e93c5aa4c15927cd9d8f7f5ebd
egrund <82370915+egrund@users.noreply.github.com>
2023-03-10 10:32:55 +0100

    log testing

--- training.py
+++ training.py
@@ -22,7 +22,10 @@ def train_self_play_best(agent, BATCH_SIZE, iterations : int, train_writer, epsi
         # save model
         if i % 100 == 0:
             agent.save_models(i)
-            testing(agent, size = 1000, print=True)
+            rewards = testing(agent, size = 100, print=True)
+
+            with train_writer.as_default():
+                tf.summary.scalar(f"average_reward", rewards[0], step=i)
 
         # new sampling + add to buffer
         _ = sampler.sample_from_game(epsilon)
